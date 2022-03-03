96f5483d279130dc9688b5d287a1533605cb5c8f
diegocgayoso <diegoc.gayoso@gmail.com>
2022-03-03 10:50:39 -0300

    parte 1 do curso angular | fazendo versionamento

---
+++ src/app/novolivro/novolivro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component ({
+  selector:'app-novo-livro',
+  templateUrl: './novolivro.component.html',
+  styleUrls: ['./novolivro.component.css']
+})
+
+export class NovoLivroComponent {}
